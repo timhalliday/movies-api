43543edf9c084534e7504fa54a7bf9fdf37a983a
Tim Halliday <thalliday793@gmail.com>
2023-10-27 14:59:14 +0100

    Implement create/read methods

--- src/main/java/com/timhalliday/moviesapi/controller/MovieController.java
+++ src/main/java/com/timhalliday/moviesapi/controller/MovieController.java
@@ -1,16 +1,41 @@
 package com.timhalliday.moviesapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.timhalliday.moviesapi.model.Movie;
+import com.timhalliday.moviesapi.service.MovieService;
 
 @RestController // (Automatically serialises return objects into a HttpResponse)
 public class MovieController {
 		
+	@Autowired
+	private MovieService movieService; 
+	
 	// insert new movie into DB
+	@PostMapping("/movie")
+	public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie){
+		Movie savedMovie = movieService.saveMovie(movie);
+		return new ResponseEntity<>(savedMovie, HttpStatus.CREATED);
+	}
 	
 	// get a single product by ID
+	public ResponseEntity<Movie> getMovie(@RequestParam(name="movieId") long movieId) {
+		Movie movie = movieService.getMovie(movieId);
+		return new ResponseEntity<>(movie, HttpStatus.OK);
+	}
 	
 	// get all products
+	public List<Movie> getMovies() {
+		return movieService.getMovies();
+	}
 	
 	// update existing item
 	
--- src/main/java/com/timhalliday/moviesapi/service/MovieService.java
+++ src/main/java/com/timhalliday/moviesapi/service/MovieService.java
@@ -1,5 +1,27 @@
 package com.timhalliday.moviesapi.service;
 
-public class MovieService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.timhalliday.moviesapi.model.Movie;
+import com.timhalliday.moviesapi.repository.MovieRepository;
+
+public class MovieService {
+	
+	@Autowired 
+	private MovieRepository movieRepository;
+	
+	 
+	public Movie saveMovie(Movie movie) {
+		return movieRepository.save(movie);
+	}
+	
+	public Movie getMovie(long movieId) {
+		return movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException("Movie not found"));
+	}
+
+	public List<Movie> getMovies() {
+		return movieRepository.findAll();
+	}
 }
